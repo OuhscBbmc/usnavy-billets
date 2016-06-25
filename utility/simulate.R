787d6118aea35ed7cff64b9e8e77ff1f1eae00c4
Will Beasley <wibeasley@hotmail.com>
2016-06-24 23:31:52 -0500

    Simulate w/ new roster

--- utility/simulate.R
+++ utility/simulate.R
@@ -14,30 +14,31 @@ requireNamespace("dplyr") #Avoid attaching dplyr, b/c its function names conflic
 
 # ---- declare-globals ---------------------------------------------------------
 set.seed(245)
-path_out_command  <- "./data-phi-free/derived/command.csv"
+path_out_command   <- "./data-phi-free/derived/command.csv"
 path_out_officer   <- "./data-phi-free/derived/officer.csv"
 
 # ---- load-data ---------------------------------------------------------------
 # Read the CSVs
-ds_command_roster <- readr::read_csv("./data-phi-free/raw/command-roster.csv")
+ds_command_roster  <- readr::read_csv("./data-phi-free/raw/command-roster.csv")
 ds_officer_roster  <- readr::read_csv("./data-phi-free/raw/officer-roster.csv")
 
 # ---- tweak-data --------------------------------------------------------------
-
+ds_officer_roster <- ds_officer_roster %>%
+  dplyr::select(-officer_name_last)
 
 # ---- expand ------------------------------------------------------------------
 # tidyr::nesting(ds_command_roster$command_id  , ds_command_roster$command_name      ),
 # tidyr::nesting(ds_officer_roster$officer_id, ds_officer_roster$office_name_last)
 
 ds_command <- expand.grid(
-  command_id  =  ds_command_roster$command_id,
+  command_id   =  ds_command_roster$command_id,
   officer_id   =  ds_officer_roster$officer_id,
   stringsAsFactors = FALSE
 )
 
 ds_officer <- expand.grid(
   officer_id   =  ds_officer_roster$officer_id,
-  command_id  =  ds_command_roster$command_id,
+  command_id   =  ds_command_roster$command_id,
   stringsAsFactors = FALSE
 )
 
@@ -47,7 +48,9 @@ ds_command <- ds_command %>%
     preference = base::sample(length(ds_officer_roster$officer_id))
   ) %>%
   dplyr::ungroup() %>%
-  dplyr::arrange(command_id)
+  dplyr::arrange(command_id) %>%
+  dplyr::left_join(ds_command_roster, by="command_id") %>%
+  dplyr::left_join(ds_officer_roster, by="officer_id")
 
 ds_officer <- ds_officer %>%
   dplyr::group_by(officer_id) %>%
@@ -55,7 +58,9 @@ ds_officer <- ds_officer %>%
     preference = base::sample(length(ds_command_roster$command_id))
   ) %>%
   dplyr::ungroup() %>%
-  dplyr::arrange(officer_id)
+  dplyr::arrange(officer_id) %>%
+  dplyr::left_join(ds_officer_roster, by="officer_id") %>%
+  dplyr::left_join(ds_command_roster, by="command_id")
 
 # ---- save-to-disk ------------------------------------------------------------
 readr::write_csv(ds_command , path_out_command)
